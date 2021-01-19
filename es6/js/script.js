5702a0536b39d04f3d04380907b6b85178854ac2
Lorenzo-Arnau <skyline03@hotmail.it>
2021-01-19 15:52:34 +0100

    1 ex done in es6 and vanilla,2ex done in vanilla

--- es6/js/script.js
+++ es6/js/script.js
@@ -24,5 +24,48 @@ for (let i = 1; i < listaBiciclette.length; i++) {
 }
 console.log(
   `
-  il peso è di ${biciLight.peso} kg
+  il peso della bici leggera è di ${biciLight.peso} kg
   `);
+
+  // Snack 2
+//   Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+  function randomNumber(min, max) {
+    let result = Math.floor(Math.random() * (max + 1 - min) + min);
+    return result;
+  }
+  let falli = randomNumber(0, 20);
+  let punti = randomNumber(0, 20);
+
+  var listaSquadre = [
+    {
+      nome: 'Lazio',
+      punti,
+      falli,
+    },
+    {
+      nome: 'Roma',
+      punti,
+      falli,
+    },
+    {
+      nome: 'Genoa',
+      punti,
+      falli,
+    },
+    {
+      'nome': 'Atalanta',
+      punti,
+      falli,
+    },
+  ];
+
+
+  for (var i = 0; i < listaSquadre.length; i++) {
+   let arrayNomiPunti = [];
+   arrayNomiPunti.push(listaSquadre[i]{nome,falli})
+  }
+  console.log(arrayNomiPunti);
--- vanilla/js/script.js
+++ vanilla/js/script.js
@@ -23,3 +23,47 @@ for (var i = 1; i < listaBiciclette.length; i++) {
   }
 }
 console.log(biciLight);
+
+
+// jsnack 2
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+function randomNumber(min, max) {
+  var result = Math.floor(Math.random() * (max + 1 - min) + min);
+  return result;
+}
+
+
+var listaSquadre = [
+  {
+    'nome': 'Lazio',
+    'punti': randomNumber(0, 20),
+    'falli' : randomNumber(0, 20),
+  },
+  {
+    'nome': 'Roma',
+    'punti': randomNumber(0, 20),
+    'falli' : randomNumber(0, 20),
+  },
+  {
+    'nome': 'Genoa',
+    'punti': randomNumber(0, 20),
+    'falli' : randomNumber(0, 20),
+  },
+  {
+    'nome': 'Atalanta',
+    'punti': randomNumber(0, 20),
+    'falli' : randomNumber(0, 20),
+  },
+];
+
+var arrayNomiPunti = [];
+for (var i = 0; i < listaSquadre.length; i++) {
+  arrayNomiPunti.push(listaSquadre[i].nome);
+  arrayNomiPunti.push(listaSquadre[i].punti);
+}
+console.log(arrayNomiPunti);
